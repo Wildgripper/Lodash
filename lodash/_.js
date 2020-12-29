742daa18f869adeefdb4d308e508c6096c522761
Petar Manoilov <54780012+Wildgripper@users.noreply.github.com>
2020-12-30 00:05:37 +0200

    Add files via upload

--- lodash/_.js
+++ lodash/_.js
@@ -1,13 +1,48 @@
 const _ = {
   clamp(number, lower, upper) {
-      number = Math.max(number, lower);
-      number = Math.min(number, upper);
-      return number;
+    number = Math.max(number, lower);
+    number = Math.min(number, upper);
+    return number;
   },
-  
+  inRange(number, start, end) {
+      if(end === undefined) {
+          end = start;
+          start = 0;
+      } else if(start > end) {
+          let temp = end;
+          end = start;
+          start = temp;
+      }
+    return start <= number && number < end;
+  },
+  words(string) {
+    return string.split(' ');
+  },
+  pad(string, number) {
+    let pad = number - string.length;
+    let startPad = Math.floor(pad / 2);
+    let endPad = Math.ceil(pad / 2);
+    string = string.padStart(startPad + string.length);
+    string = string.padEnd(endPad + string.length);
+    return string;
+  },
+  has(obj, key) {
+    if(obj[key] === undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  },
 };
 
+const myobj = {
+  // mykey: ''
+}
 // console.log(_.clamp(5, 2, 4));
+// console.log(_.inRange(5, 10, 4));
+// console.log(_.words('fred, barney, & pebbles'));
+// console.log(_.pad('hi', 1));
+console.log(_.has(myobj, 'mykey'));
 
 // Do not write or modify code below this line.
 module.exports = _;
